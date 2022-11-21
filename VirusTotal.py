ea49036260dbd81c7a630fea7edc5c9c71fcf31a
Dax <daxtonwirth@gmail.com>
2022-11-20 21:50:13 -0800

    added xfe

--- VirusTotal.py
+++ VirusTotal.py
@@ -1,31 +1,41 @@
 import requests
+from requests.auth import HTTPBasicAuth
 import json
+import base64
 
 import API_Keys
-
 VirusTotal_API_Key = API_Keys.VirusTotal_API_Key
 AbuseIPDB_API_KEY = API_Keys.AbuseIPDB_API_KEY
+XFE_API_KEY = API_Keys.IBMXForce_API_KEY
+XFE_API_PASS = API_Keys.IBMXForce_API_PASS
+
 ipaddress = input("Enter IP address: ") 
 
 VirusTotal_Request = requests.get("https://www.virustotal.com/api/v3/ip_addresses/" + ipaddress, headers={'x-apikey': VirusTotal_API_Key}).json()
-#print(request1)
-try:
-    country = VirusTotal_Request["data"]["attributes"]["country"]
-    owner = VirusTotal_Request["data"]["attributes"]["as_owner"]
-    regional_internet_registry = VirusTotal_Request["data"]["attributes"]["regional_internet_registry"]
-    harmless = VirusTotal_Request["data"]["attributes"]["last_analysis_stats"]["harmless"]
-    malicious = VirusTotal_Request["data"]["attributes"]["last_analysis_stats"]["malicious"]
-    total = int(harmless) + int(malicious)
-    reputation = VirusTotal_Request["data"]["attributes"]["reputation"]
-    cert = VirusTotal_Request["data"]["attributes"]["last_https_certificate"]["public_key"]["issuer"]
-except:
+#print(VirusTotal_Request)
+
+try: country = VirusTotal_Request["data"]["attributes"]["country"] 
+except: country = ""
+try: owner = VirusTotal_Request["data"]["attributes"]["as_owner"]
+except: owner = "N/A"
+try: regional_internet_registry = VirusTotal_Request["data"]["attributes"]["regional_internet_registry"]
+except: regional_internet_registry = ""
+try: harmless = VirusTotal_Request["data"]["attributes"]["last_analysis_stats"]["harmless"]
+except: harmless = ""
+try: malicious = VirusTotal_Request["data"]["attributes"]["last_analysis_stats"]["malicious"]
+except: malicious = ""
+try: total = int(harmless) + int(malicious)
+except: total = harmless
+try: reputation = VirusTotal_Request["data"]["attributes"]["reputation"]
+except: reputation = ""
+try:cert = VirusTotal_Request["data"]["attributes"]["last_https_certificate"]["public_key"]["issuer"]
+except KeyError:
     cert = ""
 
+print(f"\nOSINT on {ipaddress}:")
+print(f"Vendor score: {malicious}/{total} | Commun score: {reputation} | Country: {country} ({regional_internet_registry}) | Owner: {owner}")
 
-#print(f"Vendor score: {malicious}/{total}")
-#print(f"Commun score: {reputation}")
-#print(f"Country: {country} ({regional_internet_registry})")
-#print(f"Owner: {owner} ({cert})")
+#exit()
 
 url = "https://api.abuseipdb.com/api/v2/check/"
 headers={'Key': AbuseIPDB_API_KEY, 'Accept': 'application/json'}
@@ -46,4 +56,16 @@ numDistinctUsers = decodedResponse["data"]["numDistinctUsers"]
 totalReports = decodedResponse["data"]["totalReports"]
 
 
-print(f"Abuse score: {abuseConfidenceScore}/100 ({isWhitelisted}) | ISP: {isp} | Country: {countryCode} | Domain: {domain}")
+print(f"AbuseIPDB: Abuse score: {abuseConfidenceScore}/100 ({isWhitelisted}) | ISP: {isp} | Country: {countryCode} | Domain: {domain}")
+
+
+# XFE_API_KEY 
+# curl -X GET --header 'Accept: application/json' -u {API_KEY:API_PASSWORD} 'https://exchange.xforce.ibmcloud.com/api/ipr/10.10.10.10'
+#key = (XFE_API_KEY + ":" + XFE_API_KEY).encode
+#token = base64.b64encode(key)
+#XFE_Request = requests.get("https://exchange.xforce.ibmcloud.com/api/ipr/" + ipaddress, headers={'Authorization': "Basic " + token,'Accept': 'application/json'}).json()
+XFE_Request = requests.get("https://exchange.xforce.ibmcloud.com/api/ipr/" + ipaddress, auth=HTTPBasicAuth(XFE_API_KEY, XFE_API_PASS))
+#print(XFE_Request.text)
+decodedResponse = json.loads(XFE_Request.text)
+score = decodedResponse["score"]
+print(f"XFE: {score}/10")
